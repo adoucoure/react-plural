c9021b8ee0fedfdf5d09ff8627162c3b45da373e
Aboubacar Doucoure <yelounak@gmail.com>
2024-05-12 16:56:37 +0200

    fix: disabled button

--- src/button/index.js
+++ src/button/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default (props) => {
   const { children, } = props
-  return <button {...props}>
+  return <button type="button" {...props}>
     {children}
   </button>
 }
