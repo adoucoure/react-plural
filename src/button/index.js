4d086f741f2bc2060ba229105de9a41b4250f83c
Aboubacar Doucoure <yelounak@gmail.com>
2024-05-12 18:49:24 +0200

    fix: if (!process.env.BROWSER) {
    global.window = {} // Temporarily define window for server-side

--- src/button/index.js
+++ src/button/index.js
@@ -3,6 +3,10 @@ import React from 'react'
 
 export default (props) => {
   const { children, } = props
+  if (!process.env.BROWSER) {
+    return null
+  }
+
   return <button type="button" {...props}>
     {children}
   </button>
